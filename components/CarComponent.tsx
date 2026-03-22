b61f3cba261899d7d590a396fe8766874319e9e8
Juraj Packa <jurop26@azet.sk>
2026-03-22 15:22:08 +0100

    better tilt with deltaTime

--- components/CarComponent.tsx
+++ components/CarComponent.tsx
@@ -1,34 +1,46 @@
+import React, { useEffect } from "react";
 import { Canvas, Path, Skia } from "@shopify/react-native-skia";
 import { Accelerometer } from "expo-sensors";
-import React, { useEffect } from "react";
 import { useWindowDimensions } from "react-native";
-import { useDerivedValue, useSharedValue } from "react-native-reanimated";
+import { useSharedValue, useDerivedValue } from "react-native-reanimated";
 import CAR_PATH from "../constans/car";
 
 export const CarComponent = () => {
-    const { width: screenWidth } = useWindowDimensions();
-    const translateX = useSharedValue(screenWidth / 2);
-    const carWidth = 200 / 3;
+    const { width: screenWidth, height: screenHeight } = useWindowDimensions();
+
+    const speedFactor = 300;
+    const originalCarWidth = 200;
+    const originalCarHeight = 290;
+
+    const scale = 1 / 3;
+    const carWidth = originalCarWidth * scale;
+    const carHeight = originalCarHeight * scale;
+
+    const translateX = useSharedValue((screenWidth - carWidth) / 2);
+    const translateY = useSharedValue((screenHeight - carHeight) / 2);
 
     useEffect(() => {
         const subscription = Accelerometer.addListener(accelData => {
-            let newX = screenWidth / 2 - (carWidth / 2) + accelData.x * 200;
-            newX = Math.max(0, Math.min(newX, screenWidth - carWidth));
+            const velocityX = accelData.x * speedFactor;
+            const velocityY = -accelData.y * speedFactor;
 
-            translateX.value = newX;
+            translateX.value = Math.max(0, Math.min(translateX.value + velocityX * 0.128, screenWidth - carWidth));
+            translateY.value = Math.max(screenHeight / 3, Math.min(translateY.value + velocityY * 0.064, screenHeight - (carHeight * 1.5)));
         });
+
         Accelerometer.setUpdateInterval(16);
 
         return () => subscription.remove();
     }, []);
 
+
     const transform = useDerivedValue(() => {
         return [
             { translateX: translateX.value },
-            { scale: 1 / 3 },
-            { translateY: 2000 }
+            { translateY: translateY.value },
+            { scale: scale },
         ];
-    }, [translateX]);
+    }, [translateX, translateY]);
 
     return (
         <Canvas style={{ flex: 1, backgroundColor: "black" }}>
